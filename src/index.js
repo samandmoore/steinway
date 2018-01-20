c80f124f998cedecab5e4b13b9b3f58d396b05dd
Sam Moore <sam@betterment.com>
2018-01-20 15:29:59 -0500

    basic console output of trains going into manhatttan

---
+++ src/index.js
@@ -0,0 +1,40 @@
+const Mta = require('mta-gtfs')
+const moment = require('moment-timezone')
+
+const api_token = process.env['MTA_TOKEN']
+const steinway_stop_id = 'G19'
+const nqrw_feed_id = 16
+const bdfm_feed_id = 21
+const ace_feed_id = 26
+
+let mta = new Mta({
+  key: api_token,
+  feed_id: nqrw_feed_id
+})
+
+function printTrain(train) {
+  arrivalTime = moment.tz(train.arrivalTime*1000, "America/New_York")
+  console.log(`${train.routeId} arriving ${arrivalTime.fromNow()}`)
+}
+
+function printTrains(trains) {
+  trains.slice(0, 3).forEach(function (train) {
+    printTrain(train)
+  })
+}
+
+function printDirection(direction) {
+  mta.schedule(steinway_stop_id, nqrw_feed_id).then((result) => {
+    printTrains(result.schedule[steinway_stop_id][direction])
+  })
+
+  mta.schedule(steinway_stop_id, bdfm_feed_id).then((result) => {
+    printTrains(result.schedule[steinway_stop_id][direction])
+  })
+
+  mta.schedule(steinway_stop_id, ace_feed_id).then((result) => {
+    printTrains(result.schedule[steinway_stop_id][direction])
+  })
+}
+
+printDirection('S')
